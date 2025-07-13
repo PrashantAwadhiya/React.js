71cdd89e5aa48836431c8181713ba6253a841c7e
Prashant Awadhiya <rjnboss7@gmail.com>
2025-07-13 19:08:11 +0530

    minor updates

--- MovieAppReact/src/components/About.jsx
+++ MovieAppReact/src/components/About.jsx
@@ -13,17 +13,17 @@ const About = () => {
        </span>
       <h1 className="text-3xl font-bold mb-4">About This App</h1>
       <p className="text-lg mb-4">
-        Welcome to MovieApp, your ultimate destination for discovering the latest and greatest movies.
+        Welcome to Showpedia, your ultimate destination for discovering the latest and greatest movies.
         Our platform provides real-time updates on trending and popular movies, complete with detailed
         information, ratings, and reviews.
       </p>
       <p className="text-lg mb-4">
-        MovieApp is designed for movie enthusiasts who want to stay updated on new releases, explore
+        Showpedia is designed for movie enthusiasts who want to stay updated on new releases, explore
         different genres, and find hidden gems. Whether you are a fan of action, drama, horror, or
         romance, we have something for everyone.
       </p>
       <p className="text-lg">
-        Features of MovieApp:
+        Features of Showpedia:
         <ul className="list-disc list-inside text-left mx-auto mt-2">
           <li>Real-time movie updates</li>
           <li>Detailed descriptions and ratings</li>
--- MovieAppReact/src/components/templates/Dropdown.jsx
+++ MovieAppReact/src/components/templates/Dropdown.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Dropdown = ({title, options, func}) => {
   return (
-    <div className="select">
+    <div className="select text-zinc-600 font-bold bg-blue-300 rounded-md p-2">
             <select name="format" id="format" defaultValue="0" onChange={func}>
                 <option value="0" disabled>
                     {title}
